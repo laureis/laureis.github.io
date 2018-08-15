824235915d94f429c248a058f3ecb4312d1f59a3
Laure Issa <issalaure@yahoo.fr>
2018-08-15 04:50:22 +0200

    pgepr

--- src/scenes/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class extends Component {
-  render = () => (
-    <footer>
-      <span>2018 / totally not last fm</span>
-    </footer>
-  )
-}
